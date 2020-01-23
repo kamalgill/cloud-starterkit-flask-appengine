a1e257ba7b5669e95d31f6634c81602f14836bb7
Kamal Gill <kamal@kamalgill.net>
2020-01-22 18:20:18 -0800

    Enable CORS via Flask-Cors

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ Primary Flask app
 
 """
 from flask import Flask
+from flask_cors import CORS
 
 from .api import api as api_blueprint
 from .errors import add_error_handlers
@@ -10,6 +11,7 @@ from .errors import add_error_handlers
 
 def create_app():
     app = Flask(__name__)
+    CORS(app, resources={r'/*': {'origins': '*'}})
     app.register_blueprint(api_blueprint, url_prefix='/api/v1')
     add_error_handlers(app)
     return app
